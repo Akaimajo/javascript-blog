5609b78dd6d2b2941df3dae4a8645a6baf0cb80b
Paulina Walaszczyk <paulaszczyk@gmail.com>
2020-10-15 11:53:17 +0200

    add console.log for event

--- .history/js/script_20201015115112.js
+++ .history/js/script_20201015115112.js
@@ -6,7 +6,7 @@
 
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
-  console.log(event);
+  console.log('event');
 
   /* remove class 'active' from all article links  */
 
